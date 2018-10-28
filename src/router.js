7027c335089a236df885e4946659936de8d74dcc
johnt1000 <jonatas.rodriguess@gmail.com>
2018-10-28 01:40:51 -0300

    Removendo lixo.

--- src/router.js
+++ src/router.js
@@ -1,29 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Dashboard from './views/Dashboard.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/home',
-      name: 'dashboard',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
       path: '/',
       name: 'login',
       component: Login
     },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    }
   ]
 })
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <GridIcons></GridIcons>
+</template>
+
+<script>
+import GridIcons from '@/components/dashboard/GridIcons.vue'
+
+export default {
+  name: 'login',
+  components: {
+    GridIcons
+  }
+}
+</script>
