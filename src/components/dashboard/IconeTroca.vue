50580b493853f1e883404a7db711eb756f533797
johnt1000 <jonatas.rodriguess@gmail.com>
2018-12-06 00:41:33 -0300

    [UPD] Finalizado card de troca

--- src/components/dashboard/IconeTroca.vue
+++ src/components/dashboard/IconeTroca.vue
@@ -11,13 +11,13 @@
         <v-card-title primary-title>
           <div>
             <div class="headline">Troca Solidária</div>
-            <span class="grey--text">Cadastre seus medicamentos</span>
+            <span class="grey--text">Cadastre seus medicamentos para troca e ache medicamentos que vocé precisa.</span>
           </div>
         </v-card-title>
 
         <v-card-actions>
-          <v-btn flat>Lista</v-btn>
-          <v-btn flat color="purple">Novo Cadastro</v-btn>
+          <v-btn flat>Lista de Trocas</v-btn>
+          <v-btn flat color="purple">Nova Troca</v-btn>
           <v-spacer></v-spacer>
           <v-btn icon @click="show = !show">
             <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
@@ -26,7 +26,14 @@
 
         <v-slide-y-transition>
           <v-card-text v-show="show">
-            Cadastre seus medicamentos de uso continuo ou transitório.
+            Aute sint ex voluptate cupidatat tempor
+            reprehenderit ad magna qui reprehenderit
+            veniam cillum et aliqua. Tempor minim amet
+            elit cillum id. Eu deserunt Lorem veniam
+            quis pariatur officia voluptate. Et sint
+            nulla consequat amet enim pariatur. Mollit
+            consequat elit irure ipsum cillum commodo
+            ipsum veniam Lorem.
           </v-card-text>
         </v-slide-y-transition>
       </v-card>
